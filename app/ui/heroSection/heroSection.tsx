46052c8415a0984d7285288f1ee0aade23ff9f7a
k-rahul21 <mrkhandelwal19@gmail.com>
2024-05-19 12:57:25 +0530

    feat: working on herosection

---
+++ app/ui/heroSection/heroSection.tsx
@@ -0,0 +1,17 @@
+const HeroSection = () => {
+    return( 
+        <section className="hero-section mt-10">  
+            <div className="parent-div">
+                <div className="left-child">
+                    <h2>मेरा परिचय</h2>
+                    <h1>Hello, my name's Rahul.</h1>
+                    <p>Experienced in building robust web application.</p>
+                    <button>Let's Talk</button>
+                </div>
+                <div className="right-child">right</div> 
+            </div>
+        </section>
+    )
+}
+
+export default HeroSection
